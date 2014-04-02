d3ea5e6f46bdad3ded3f5a21308e3192a1df50ed
Ziyu Zhang <zz31@calydon.pratt.duke.edu>
2014-04-02 13:52:28 -0400

    wrap up PlateHole v1.0

--- src/base/PlateholeApp.C
+++ src/base/PlateholeApp.C
@@ -7,6 +7,8 @@
 #include "Sig11Func.h"
 #include "Sig12Func.h"
 #include "Sig22Func.h"
+#include "Disp1Func.h"
+#include "Disp2Func.h"
 
 template<>
 InputParameters validParams<PlateholeApp>()
@@ -18,8 +20,8 @@ InputParameters validParams<PlateholeApp>()
 PlateholeApp::PlateholeApp(const std::string & name, InputParameters parameters) :
     MooseApp(name, parameters)
 {
-  srand(libMesh::processor_id());
-  
+  srand(processor_id());
+
   Moose::registerObjects(_factory);
   ModulesApp::registerObjects(_factory);
   PlateholeApp::registerObjects(_factory);
@@ -45,6 +47,8 @@ PlateholeApp::registerObjects(Factory & factory)
   registerFunction(Sig11Func); // register ZZY function
   registerFunction(Sig12Func); // register ZZY function
   registerFunction(Sig22Func); // register ZZY function
+  registerFunction(Disp1Func);
+  registerFunction(Disp2Func);
 }
 
 void
